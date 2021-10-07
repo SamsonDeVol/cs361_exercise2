c78762de24dac534b8b4f17682a998577f3abe9a
SamsonDeVol <devols@oregonstate.edu>
2021-10-07 09:34:22 -0700

    add name parameter to functions

--- main.rb
+++ main.rb
@@ -1,17 +1,17 @@
 @name = "Johanna Jackson"
 
-def add_borg_to_name()
-  reversed_name = name_swapper()
+def add_borg_to_name(name)
+  reversed_name = name_swapper(name)
   borged_name = reversed_name << " Borg"
   return borged_name
 end
 
-def name_swapper()
-  split_name = @name.split
+def name_swapper(name)
+  split_name = name.split
   reversed_split_name = split_name.reverse
   swapped_name = reversed_split_name.join(' ')
   return swapped_name
 end
 
-puts "New name: #{name_swapper()}"
-puts "New borg name: #{add_borg_to_name()}"
+puts "New name: #{name_swapper(@name)}"
+puts "New borg name: #{add_borg_to_name(@name)}"
